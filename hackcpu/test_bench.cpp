f8ec1ba2978670f368c8d2190f509ebe82cd5658
tohshima <tetsu.ohshima@gmail.com>
2024-07-13 00:07:56 +0900

    Changed bus protocol of debug port to axis.

--- hackcpu/test_bench.cpp
+++ hackcpu/test_bench.cpp
@@ -31,37 +31,33 @@ int main() {
 
     // CPU interface signals
     ap_uint<1> reset = 1;
-    ap_uint<1> write_out;
-    word_t outM;
-    addr_t addressM;
-    word_t pc;
-    word_t debug_A;
-    word_t debug_D;
-    word_t debug_ALU;
-    word_t debug_instruction;
+    hls::stream<debug_t> debug;
 
     // Reset CPU
-    cpu(ROM, reset, write_out, outM, addressM, pc, debug_A, debug_D, debug_ALU, debug_instruction);
+    cpu(ROM, reset, debug);
 
     // Run simulation
     reset = 0;
-    for (int cycle = 0; cycle < 100; ++cycle) {
+    //for (int cycle = 0; cycle < 100; ++cycle) {
 
         // Run CPU cycle
-        cpu(ROM, reset, write_out, outM, addressM, pc, debug_A, debug_D, debug_ALU, debug_instruction);
+        cpu(ROM, reset, debug);
 
         // Print CPU state
-        char buf[512];
-        sprintf(buf, "Cycle %6d: PC=%04x, A=%04x, D=%04x, ALU=%04x, Inst=%04x",
-            cycle, pc.to_ushort(),debug_A.to_ushort(),debug_D.to_ushort(), 
-            debug_ALU.to_ushort(), debug_instruction.to_ushort());
-        std::cout << buf;
-        if (write_out) {
-            sprintf(buf, "Write %04x to RAM[%04x]",outM.to_ushort(), addressM.to_ushort());
+        while (!debug.empty()) {
+            char buf[512];
+            debug_t dinfo = debug.read();
+            sprintf(buf, "Cycle %6d: PC=%04x, A=%04x, D=%04x, ALU=%04x, Inst=%04x",
+                dinfo.data.cycle, dinfo.data.pc.to_ushort(),dinfo.data.regA.to_ushort(),dinfo.data.regD.to_ushort(), 
+                dinfo.data.alu_out.to_ushort(), dinfo.data.instruction.to_ushort());
             std::cout << buf;
+            if (dinfo.data.write_out) {
+                sprintf(buf, " Write %04x to RAM[%04x]",dinfo.data.outM.to_ushort(), dinfo.data.addressM.to_ushort());
+                std::cout << buf;
+            }
+            std::cout << std::endl;
         }
-        std::cout << std::endl;
-    }
+    //}
 
     return 0;
 }
