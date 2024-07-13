9e4cd0040cc38a76bfa924b54ef237d552616b4e
tohshima <tetsu.ohshima@gmail.com>
2024-07-13 18:20:19 +0900

    Reapply "Changed bus protocol of debug port to axis."
    This reverts commit 0f856b5b587eba1f21ceadf93f8f03b027662b3c.

--- hackcpu/hackcpu.hpp
+++ hackcpu/hackcpu.hpp
@@ -1,4 +1,5 @@
 #include <ap_int.h>
+#include <cstdint>
 #include <hls_stream.h>
 
 // Define bit widths
@@ -16,6 +17,20 @@ typedef ap_uint<ADDR_WIDTH> addr_t;
 #include <ap_axi_sdata.h>
 typedef ap_axiu<WORD_WIDTH, 0, 0, 0> axi_word_t;
 
+// For debug
+typedef struct {
+    uint32_t    cycle;
+    ap_uint<1>  write_out;
+    word_t      outM;
+    addr_t      addressM;
+    addr_t      pc;
+    word_t      regA;
+    word_t      regD;
+    word_t      alu_out;
+    word_t      instruction;
+} debug_s;
+
+typedef hls::axis<debug_s, 0,0,0> debug_t;
+
 void cpu(word_t rom[1 << ADDR_WIDTH],
-         ap_uint<1>& reset, ap_uint<1>& write_out, word_t& outM, addr_t& addressM, word_t& pc,
-         word_t& debug_A, word_t& debug_D, word_t& debug_ALU, word_t& debug_instruction);
+         ap_uint<1>& reset, hls::stream<debug_t>& debug);
